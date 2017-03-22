182517d3dab7cd007b8e833eb166c406ec387e5a
Juan Angel Arias A. <arias@ariasmunguia.com>
2017-03-22 09:22:02 -0600

    webapi_01

---
+++ Intermoda.WebApi.LbDatPro/Controllers/LecturaCuponController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Http;
+using Intermoda.Business.LbDatPro;
+
+namespace Intermoda.WebApi.LbDatPro.Controllers
+{
+    public class LecturaCuponController : ApiController
+    {
+        public LecturaCuponBusiness ValidaUsuario(string user)
+        {
+            var resp = LecturaCuponBusiness.UsuarioLecturaCupon(user);
+
+            return resp;
+        }
+
+
+        public LecturaCuponBusiness LecturaCupon(string cupon, string user)
+        {
+            var resp = LecturaCuponBusiness.LecturaCupon(cupon, user);
+
+            return resp;
+        }
+    }
+}
